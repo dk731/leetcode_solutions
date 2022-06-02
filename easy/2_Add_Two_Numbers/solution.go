83e179b92dab95c20d2336b57fa1faf5978d4387
Demids Kaidalovs <demid.kaidalov@gmail.com>
2022-06-02 23:30:42 +0300

    Finished 2. Add Two Numbers

---
+++ easy/2_Add_Two_Numbers/solution.go
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/add-two-numbers/
+package main
+
+import "fmt"
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	resHead := &ListNode{0, nil}
+	resP := resHead
+	p1 := l1
+	p2 := l2
+	overflow := 0
+
+	for p1 != nil || p2 != nil {
+		resP.Next = &ListNode{0, nil}
+		resP = resP.Next
+
+		tempVal := overflow
+		overflow = 0
+
+		if p1 != nil {
+			tempVal += p1.Val
+			p1 = p1.Next
+		}
+
+		if p2 != nil {
+			tempVal += p2.Val
+			p2 = p2.Next
+		}
+
+		resP.Val = tempVal % 10
+		if tempVal > 9 {
+			overflow = 1
+		}
+	}
+
+	if overflow > 0 {
+		resP.Next = &ListNode{overflow, nil}
+	}
+
+	return resHead.Next
+}
+
+func main() {
+	num1 := ListNode{2, &ListNode{4, &ListNode{3, nil}}}
+	num2 := ListNode{5, &ListNode{6, &ListNode{4, nil}}}
+
+	res := addTwoNumbers(&num1, &num2)
+	for res != nil {
+		fmt.Println(res.Val)
+		res = res.Next
+	}
+
+}
